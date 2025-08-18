542270429f8d427fa14cc87ff091fa79b07d2f9c
Warren Hunt <wmatthunt@gmail.com>
2025-08-18 17:00:39 -0400

    Update emotion_detection.py

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -6,4 +6,30 @@ def emotion_detector(text_to_analyse):  # Define a function emotion_detector tha
     myobj = { "raw_document": { "text": text_to_analyse } }  # Create a dictionary with the text to be analyzed
     header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
     response = requests.post(url, json = myobj, headers=header)  # Send a POST request to the API with the text and headers
-    return response.text  # Return the response text from the API
+    
+    formatted_response = json.loads(response.text)
+
+    anger_score = formatted_response['emotionPredictions'][0]['emotion']['anger']
+    disgust_score = formatted_response['emotionPredictions'][0]['emotion']['disgust']
+    fear_score = formatted_response['emotionPredictions'][0]['emotion']['fear']
+    joy_score = formatted_response['emotionPredictions'][0]['emotion']['joy']
+    sadness_score = formatted_response['emotionPredictions'][0]['emotion']['sadness']
+    
+    emotions =   {'anger':anger_score,
+    'disgust':disgust_score,
+     'fear':fear_score,
+     "joy":joy_score,
+     "sadness":sadness_score,
+     }
+
+    highest_emotion = max(emotions, key=emotions.get)
+
+    results =   {'anger':anger_score,
+    'disgust':disgust_score,
+     'fear':fear_score,
+     "joy":joy_score,
+     "sadness":sadness_score,
+     "dominant_emotion":highest_emotion
+     }
+    return results
+  
