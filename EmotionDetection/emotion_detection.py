cf83ebcab26e645cf8f44b87bd7d3282533e0025
Warren Hunt <wmatthunt@gmail.com>
2025-08-19 09:27:36 -0400

    Update emotion_detection.py

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -6,23 +6,33 @@ def emotion_detector(text_to_analyse):  # Define a function emotion_detector tha
     myobj = { "raw_document": { "text": text_to_analyse } }  # Create a dictionary with the text to be analyzed
     header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
     response = requests.post(url, json = myobj, headers=header)  # Send a POST request to the API with the text and headers
-    
-    formatted_response = json.loads(response.text)
 
-    anger_score = formatted_response['emotionPredictions'][0]['emotion']['anger']
-    disgust_score = formatted_response['emotionPredictions'][0]['emotion']['disgust']
-    fear_score = formatted_response['emotionPredictions'][0]['emotion']['fear']
-    joy_score = formatted_response['emotionPredictions'][0]['emotion']['joy']
-    sadness_score = formatted_response['emotionPredictions'][0]['emotion']['sadness']
     
-    emotions =   {'anger':anger_score,
-    'disgust':disgust_score,
-     'fear':fear_score,
-     "joy":joy_score,
-     "sadness":sadness_score,
-     }
+    if response.status_code == 200:
+        formatted_response = json.loads(response.text)
+        anger_score = formatted_response['emotionPredictions'][0]['emotion']['anger']
+        disgust_score = formatted_response['emotionPredictions'][0]['emotion']['disgust']
+        fear_score = formatted_response['emotionPredictions'][0]['emotion']['fear']
+        joy_score = formatted_response['emotionPredictions'][0]['emotion']['joy']
+        sadness_score = formatted_response['emotionPredictions'][0]['emotion']['sadness']
 
-    highest_emotion = max(emotions, key=emotions.get)
+        emotions =   {'anger':anger_score,
+        'disgust':disgust_score,
+        'fear':fear_score,
+        "joy":joy_score,
+        "sadness":sadness_score,
+        }
+        highest_emotion = max(emotions, key=emotions.get)
+    
+    elif response.status_code == 400:
+        anger_score = None
+        disgust_score = None
+        fear_score = None
+        joy_score = None
+        sadness_score = None
+        highest_emotion = None
+
+  
 
     results =   {'anger':anger_score,
     'disgust':disgust_score,
